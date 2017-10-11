ef6cd4cd760fedd5f7b1c0ebc1dd4ea7b73ce123
Stephany Choi <s.choi417@gmail.com>
2017-10-11 14:29:46 +0000

    Add wiki model

---
+++ spec/models/wiki_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Wiki, type: :model do
+    let(:user) { User.create!(email: "bloc@bloc.com", password: "password") }
+    let(:wiki) { Wiki.create!(title: "Wiki Title", body: "This is the Wiki body.", user: user) }
+    
+    it { is_expected.to belong_to(:user) }
+    
+    describe "attributes" do
+       it "has title, and body attributes" do
+           p user
+           expect(wiki).to have_attributes(title: "Wiki Title", body: "This is the Wiki body.")
+       end
+       
+    end
+end
