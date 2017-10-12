a6bc17722cda591ca79cd5fa1b4db2ecf347641d
Stephany Choi <s.choi417@gmail.com>
2017-10-12 15:12:30 +0000

    Default to false for wiki private

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20171011140132) do
   create_table "wikis", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.boolean "private"
+    t.boolean "private", default: false
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
