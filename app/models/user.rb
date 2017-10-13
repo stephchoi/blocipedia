ef2f2042072d76d29a1a9288cdef44f3256e9b9a
Stephany Choi <s.choi417@gmail.com>
2017-10-13 14:49:06 +0000

    Add user roles and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
          
   has_many :wikis
+  
+  before_save { self.role ||= :member }
+  enum role: [:member, :admin]
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+    let(:user) { User.create!(email: "bloc@bloc.com", password: "password") }
+    
+    describe "attributes" do
+       it "has email and password attributes" do
+           expect(user).to have_attributes(email: "bloc@bloc.com", password: "password")
+       end
+       
+       it "responds to role" do
+           expect(user).to respond_to(:role)
+       end
+       
+       it "responds to admin?" do
+           expect(user).to respond_to(:admin?)
+       end
+ 
+       it "responds to member?" do
+           expect(user).to respond_to(:member?)
+       end
+   end
+   
+   describe "roles" do
+       it "defaults to member role" do
+           expect(user.role).to eql("member")
+       end
+       
+       context "member user" do
+           it "returns true for #member?" do
+               expect(user.member?).to be_truthy
+           end
+           
+           it "returns false for #admin?" do
+               expect(user.admin?).to be_falsey
+           end
+       end
+
+       context "admin user" do
+           before do
+               user.admin!
+           end
+           
+           it "returns false for #member?" do
+               expect(user.member?).to be_falsey
+           end
+           
+           it "returns true for #admin?" do
+               expect(user.admin?).to be_truthy
+           end
+       end
+    end
 end
